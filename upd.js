06956acf427752367e4741ae627dc969806a2cec
Slingexe <slingexegithub@gmail.com>
2025-01-21 18:03:51 +0000

    Now checks for updates on startup

---
+++ upd.js
@@ -0,0 +1,15 @@
+var checkUpdate = require('check-update-github');
+var pkg = require('./package.json');
+
+checkUpdate({
+    name: pkg.name, 
+    currentVersion: pkg.version, 
+    user: 'slingexe',
+    branch: 'main'
+    }, function(err, latestVersion, defaultMessage){
+    if(!err){
+        console.log(defaultMessage);
+    }
+});
+
+module.exports.checkUpdate = checkUpdate
